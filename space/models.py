3d34f2054e16d174244ebaf5cd170d1ce9bbc072
Colten Bayles <cbayles@outlook.com>
2023-10-07 11:15:55 -0400

    Added Jupiter & Moon Model DID not add image reference. Will need to go back and do that once templates are made

--- space/models.py
+++ space/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+# Planetary & Moon Models
+class Jupiter(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    diameter = models.FloatField(help_text="Diameter in kilometers")
+    distance_from_sun = models.FloatField(help_text="Distance from Sun in Astronomical units")
+    mass = models.FloatField(help_text="Mass relected in Earth masses")
+    orbital_period = models.FloatField(help_text="Orbital period reflected in Earth days")
+    rotation_period = models.FloatField(help_text="Rotational period reflected in hours")
+
+class Moon(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    diameter = models.FloatField(help_text="Diameter in Kilometer")
+    orbital_period = models.FloatField(help_text="Orbital period reflected in Earth days")
+    distance_from_jupiter = models.FloatField(help_text="Average distance measured in kilometers")
+    
