2be3bcfd17f8273edf69f50c25f9ae4e795b84c6
Colten Bayles <cbayles@outlook.com>
2023-10-08 13:51:02 -0400

    Added testing logic in views

--- space/views.py
+++ space/views.py
@@ -3,6 +3,9 @@ from .forms import RegisterForm
 from .models import Moon
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.views.decorators.csrf import csrf_exempt
+
 # Create your views here.
 def main(request):
     return render(request, 'space/main.html')
@@ -33,3 +36,28 @@ def user_login(request):
         else:
             return render(request, 'space/main.html', {'error': 'Invalid username or password'})
     return render(request, 'space/main.html')
+
+#Testing Logic - ***FOR DEVELOPMENT ONLY***
+# def register_view(request):
+#     if request.method == "POST":
+#         data = json.loads(request.body.decode("utf-8"))
+        
+#         username = data.get("username")
+#         password = data.get("password")
+#         email = data.get("email")
+
+#         if not username or not password or not email:
+#             return JsonResponse({"error": "Missing required parameters"}, status=400)
+
+  
+#         if User.objects.filter(username=username).exists():
+#             return JsonResponse({"error": "Username already exists"}, status=400)
+
+    
+#         user = User(username=username, email=email)
+#         user.set_password(password)
+#         user.save()
+
+#         return JsonResponse({"success": "User registered successfully"})
+
+#     return JsonResponse({"error": "Invalid request method"}, status=405)
